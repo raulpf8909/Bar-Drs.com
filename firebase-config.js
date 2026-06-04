c603cbfa93a54bd1c338f546ab2789e9740ef7c3
Raul Perez <raulpf8909@gmail.com>
2026-06-04 13:13:10 -0400

    chore: update and synchronize project node_modules dependencies

---
+++ firebase-config.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Reemplaza esto con la configuración de tu proyecto Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCPkfsrWoSkF7oYE_QAKkjJ5oYLzsXynao",
+  authDomain: "bardrs-64b37.firebaseapp.com",
+  projectId: "bardrs-64b37",
+  storageBucket: "bardrs-64b37.firebasestorage.app",
+  messagingSenderId: "437026154305",
+  appId: "1:437026154305:web:7ef5635e69acfa95a623c0",
+  measurementId: "G-PXEQ13C50F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firebase services
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, provider, db, storage };
