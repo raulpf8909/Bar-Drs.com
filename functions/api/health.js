31f6ba016e37e0fdc16cd7c2b583820f38c68f19
Raul Perez <raulpf8909@gmail.com>
2026-06-11 11:47:01 -0400

    test: add health endpoint to check Cloudflare deployment

---
+++ functions/api/health.js
@@ -0,0 +1,5 @@
+export async function onRequestPost() {
+  return new Response(JSON.stringify({ ok: true }), {
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+  });
+}
